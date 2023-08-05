e7783c9f30b414a413ebbf4e4e5fddf4c9f8b977
alexthomas <alexjosthomas@gmail.com>
2023-08-04 17:45:51 -0700

    rename react front end for safekeping

---
+++ react_search_front_end/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+var FontAwesome = require('react-fontawesome');
+
+
+function Navbar() {
+    return (
+        
+        <nav className="navbar fixed-top navbar-expand-lg " id="sectionsNav">
+        <div className="container">
+          <div className="navbar-translate">
+            <a className="navbar-brand" href="">
+              Twitter Search Engine </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://github.com/athom031/TwitterCrawlAndSearch">
+                    Github Repo
+                    <FontAwesome
+                        className='fab fa-github'
+                        name='github'
+                        size='2x'
+                        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                    />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
